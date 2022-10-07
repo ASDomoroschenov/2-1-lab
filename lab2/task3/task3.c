35d08b1842bae364b8532d8d9738887cc0b2c784
Alexandr <ASDomoroschenov@mail.ru>
2022-10-07 14:34:27 +0300

    task3

--- lab2/task3/task3.c
+++ lab2/task3/task3.c
@@ -2,81 +2,108 @@
 #include <stdlib.h>
 #include <math.h>
 
-double my_pow(double b, double n);
-double exp_limit(int l);
-int factorial (int n);
-double exp_series(int l);
-double function(double x);
-double exp_equation(double (*func)(double), double begin, double end, int l);
+#define pi 3.1415926535897
+
+long long factorial (int n);
+double exp_limit(double accuracy);
+double exp_range(double accuracy);
+double func_exp(double x);
+double pi_limit(double accuracy);
+double pi_range(double accuracy);
+double func_pi(double x);
+double dichotomy_method(double (*func)(double), double begin, double end, double accuracy);
+
 
 int main() {
-	printf("limit : %.10lf\n", exp_limit(7));
-	printf("series : %.10lf\n", exp_series(7));
-	printf("equation : %.10lf\n", exp_equation(function, (double)2.0, (double)3.0, 7));
-	printf("original : %.10lf\n", exp(1));
+	printf("%.20lf\n", dichotomy_method(func_pi, (double)3.0, (double)3.5, 0.0001));
 
 	return 0;
 }
 
-double exp_limit(int l) {
-	double accuracy = pow(10, -l);
+long long factorial(int n) {
+  return (n < 2) ? 1 : n * factorial (n - 1);
+}
+
+double exp_limit(double accuracy) {
 	double prev = 0.0;
-	double now = 0.0;
+	double now = 2.0;
+	int i = 2;
+
+	do {
+		prev = now;
+		now = pow(1 + 1.0 / i, i); 
+		i++;
+	} while (abs(now - prev) > accuracy);
+
+	return now;
+}
+
+double exp_range(double accuracy) {
+	double now = 1.0;
+	double prev = 0.0;
 	int i = 1;
 
 	do {
-		prev = pow(1.0 + 1.0 / i, i);
-		now = pow(1.0 + 1.0 / (i + 1), i + 1);
-		printf("%.10lf\n", now - prev);
-		i++;
-	} while (fabs(now - prev) >= accuracy);
+		prev = now;
+		now += 1.0 / factorial(i);
+		i++; 
+	} while (now - prev > accuracy);
 
-	return prev;
+	return now;
 }
 
-int factorial (int n) {
-  return (n < 2) ? 1 : n * factorial(n - 1);
+double func_exp(double x) {
+	return log(x) - 1.0;
 }
 
-double exp_series(int l) {
-	int i = 0;
-	double accuracy = pow(10, -l);
-	double sum = 0.0;
-
-	while(1.0 / factorial(i) > accuracy) {
-		sum += 1.0 / factorial(i++);
-	}
-
-	return sum;
-}
-
-double function(double x) {
-	return log(x) - 1;
-}
-
-double exp_equation(double (*func)(double), double begin, double end, int l) {
-	double result = 0.0;
-	double accuracy = pow(10, -l);
+double pi_limit(double accuracy) {
+	double prev = 0.0;
+	double now = 4.0;
+	int i = 2;
 
 	do {
-		result = (begin + end) / 2;
-		if (func(result) * func(begin) < 0 || fabs(func(result) * func(begin)) < accuracy) {
-			end = result;
-		} else {
-			begin = result;
-		}
-	} while(fabs(begin - end) >= accuracy);
+		prev = now;
+		now = pow(pow(2, i) * factorial(i), 4) / (i * pow(factorial(2 * i), 2)); 
+		i++;
+	} while (fabs(now - prev) > accuracy);
 
-	return result;
+	return now;
 }
 
-double binpow (double a, double n) {
-	if (n == 0)
-		return 1.0;
-	if (n % 2 == 1)
-		return binpow(a, n - 1) * a;
-	else {
-		double b = binpow(a, n / 2);
-		return b * b;
-	}
+double pi_range(double accuracy) {
+	double prev = 0.0;
+	double now = 1.0;
+	int i = 2;
+
+	do {
+		prev = now;
+		now += ((i % 2 == 0) ? (-1.0) : (1.0)) / (2 * i - 1);
+		i++; 
+	} while (fabs(now - prev) > accuracy);
+
+	return 4.0 * now;
+}
+
+double func_pi(double x) {
+	return cos(pi * x) + 1.0;
+}
+
+double dichotomy_method(double (*func)(double), double begin, double end, double accuracy) {
+	double result = 0.0;
+	
+	do {
+		result = (begin + end) / 2;
+
+		if (fabs(func(result)) < accuracy) {
+			return result;
+		} else {
+			if (func(result) * func(begin) < accuracy) {
+				end = result;
+			} else {
+				begin = result;
+			}
+		}
+	} while (fabs(begin - end) > accuracy);
+
+	return result;
 }
