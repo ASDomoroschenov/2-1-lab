3781b34e49e18eaca8acb63d98e7b33ecfc573d3
Alexandr <ASDomoroschenov@mail.ru>
2022-10-11 11:25:55 +0300

    task3 add .h file

--- lab2/task3/task3.c
+++ lab2/task3/task3.c
@@ -1,30 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-#define pi 3.14159265358979323846
-#define eps 0.000000000000000000001
-
-double factorial(double n);
-double exp_limit(double accuracy);
-double exp_range(double accuracy);
-double pi_limit(double accuracy);
-double pi_range(double accuracy);
-double ln2_limit(double accuracy);
-double ln2_range(double accuracy);
-double sqrt2_limit(double accuracy);
-double sqrt2_range(double accuracy);
-double for_gamma_limit(double k, double m);
-double gamma_limit(double accuracy);
-double gamma_range(double accuracy);
-double for_gamma_eq(double accuracy);
-double func_pi(double x);
-double func_exp(double x);
-double func_ln2(double x);
-double func_sqrt2(double x);
-double func_gamma(double x);
-double dichotomy_method(double (*func)(double), double begin, double end, double accuracy);
-int is_prime(int number);
+#include "task3.h"
 
 int main() {
 	printf("Exponent over limit: %.10lf\n", exp_limit((double)0.0000001));
